d629511a318e5d1b94e984f886aef3e7afb72199
G-agone <142501350+gmh-07@users.noreply.github.com>
2025-11-09 16:51:51 +0530

    11-09-2025 3740-minimum-distance-between-three-equal-elements-i Easy .java 44.3 MB (0%) 3 ms (100%)

---
+++ LeetCode/Easy/3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i-11-09-2025-16-51-46.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        int n = nums.length;
+        if(n<3) return -1;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                if(nums[i] != nums[j]) continue;
+                for(int k=j+1;k<n;k++){
+                    if(nums[i] == nums[j] && nums[i] == nums[k] && nums[j] == nums[k]){
+                        int sum = Math.abs(i-j)+Math.abs(j-k)+Math.abs(k-i);
+                        min = Math.min(min,sum);
+                    }
+                }
+            }
+        }
+        if(min==Integer.MAX_VALUE) return -1;
+        return min;
+    }
+}
