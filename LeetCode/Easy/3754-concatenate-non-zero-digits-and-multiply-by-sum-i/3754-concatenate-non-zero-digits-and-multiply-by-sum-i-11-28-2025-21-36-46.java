03f87447b94dca3ffc9405838a7eaa1e02dd511d
G-agone <142501350+gmh-07@users.noreply.github.com>
2025-11-28 21:36:59 +0530

    11-28-2025 3754-concatenate-non-zero-digits-and-multiply-by-sum-i Easy .java 42.6 MB (66.52%) 2 ms (56.58%)

---
+++ LeetCode/Easy/3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i-11-28-2025-21-36-46.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        if(n==0) return 0;
+        String str = String.valueOf(n);
+        StringBuilder sb = new StringBuilder("");
+        long sum = 0;
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch-'0'!=0){
+                sb.append(ch);
+                sum += (ch-'0');
+            }
+        }
+        long sbToInt = Integer.parseInt(sb.toString());
+        long ans = sbToInt*sum;
+        return ans;
+    }
+}
