b18a9d54043278e7f970fb3e779a57ba2c4d6180
G-agone <142501350+gmh-07@users.noreply.github.com>
2025-12-12 23:49:05 +0530

    Update 1812-determine-color-of-a-chessboard-square-09-23-2025-23-51-22.java

--- LeetCode/Easy/1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square-09-23-2025-23-51-22.java
+++ LeetCode/Easy/1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square-09-23-2025-23-51-22.java
@@ -3,6 +3,7 @@ class Solution {
         int a = coordinates.charAt(0)-'a';
         int b = coordinates.charAt(1)-'0';
         System.out.println(a);
+        
         System.out.println(b);
 
         if((a+b)%2==0) return true;
