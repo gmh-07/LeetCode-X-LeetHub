2f4a620f8f4f64efbfdc7bb22941a27278f4060c
G-agone <142501350+gmh-07@users.noreply.github.com>
2026-01-09 23:24:41 +0530

    01-09-2026 1123-lowest-common-ancestor-of-deepest-leaves Medium .java 46.5 MB (31.4%) 1 ms (45.12%)

---
+++ LeetCode/Medium/1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves-01-09-2026-23-24-35.java
@@ -0,0 +1,28 @@
+class Solution {
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return dfs(root).node;
+    }
+    private class Pair{
+        TreeNode node;
+        int depth;
+        Pair(TreeNode node,int depth){
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+    private Pair dfs(TreeNode root){
+        if(root==null) return new Pair(null,0);
+        Pair left = dfs(root.left);
+        Pair right = dfs(root.right);
+
+        if(left.depth == right.depth){
+            return new Pair(root,left.depth+1);
+        }
+        else if(left.depth > right.depth){
+            return new Pair(left.node,left.depth+1);
+        }
+        else{
+            return new Pair(right.node,right.depth+1);
+        }
+    }
+}
