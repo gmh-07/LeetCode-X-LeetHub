044a658c378209b92f07a80733e070870d5350d4
G-agone <142501350+gmh-07@users.noreply.github.com>
2026-01-14 15:59:42 +0530

    01-14-2026 1587-bank-account-summary-ii Easy .sql 0B (100%) 680 ms (94.06%)

---
+++ LeetCode/Easy/1587-bank-account-summary-ii/1587-bank-account-summary-ii-01-14-2026-15-59-41.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+WITH cte AS(
+    SELECT account,SUM(amount) AS balance
+    FROM Transactions
+    GROUP BY account
+)
+
+SELECT 
+    u.name AS name,
+    cte.balance
+FROM cte 
+JOIN Users u
+ON cte.account = u.account
+WHERE cte.balance > 10000;
+    
