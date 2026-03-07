8c1dfb78ca2e21b1ea928b273f637ce8d826c80a
G-agone <142501350+gmh-07@users.noreply.github.com>
2026-03-07 16:02:06 +0530

    03-07-2026 0013-roman-to-integer Easy .java N/A (0%) N/A (0%)

---
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer-03-07-2026-16-02-03.java
@@ -0,0 +1,136 @@
+// import java.util.*;
+
+// class Solution {
+//     public int romanToInt(String s) {
+
+//         HashMap<Character,Integer> map = new HashMap<>();
+//         map.put('I',1);
+//         map.put('V',5);
+//         map.put('X',10);
+//         map.put('L',50);
+//         map.put('C',100);
+//         map.put('D',500);
+//         map.put('M',1000);
+
+//         int sum = 0;
+
+//         for(int i = 0; i < s.length(); i++){
+
+//             int value = map.get(s.charAt(i));
+
+//             if(i < s.length()-1 && value < map.get(s.charAt(i+1))){
+//                 sum -= value;
+//             }
+//             else{
+//                 sum += value;
+//             }
+//         }
+
+//         return sum;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// My Solution
+// class Solution {
+//     public int romanToInt(String s) {
+//         HashMap<Character,Integer> map = new HashMap<>();
+//         map.put('I',1);
+//         map.put('V',5);
+//         map.put('X',10);
+//         map.put('L',50);
+//         map.put('C',100);
+//         map.put('D',500);
+//         map.put('M',1000);
+
+//         int sum = 0;
+//         int i = s.length()-1;
+//         while(i>=0){
+//             char ch = s.charAt(i);
+//             if(ch=='I' && i<s.length()-1){
+//                 if(s.charAt(i+1)=='V' || s.charAt(i+1)=='X') sum -= 1;
+//             }
+//             else if(ch=='X' && i<s.length()-1){
+//                 if(s.charAt(i+1)=='L' || s.charAt(i+1)=='C') sum -= 10;
+//             }
+//             else if(ch=='C' && i<s.length()-1){
+//                 if(s.charAt(i+1)=='D' || s.charAt(i+1)=='M') sum -= 100;
+//             }
+//             else {
+//                 sum += map.get(ch);
+//             }
+//             i--;
+//         }
+//         return sum;
+//     }
+// }
+
+
+
+
+
+
+
+
+// It's my approach but little fix is added subtracted flag
+class Solution {
+    public int romanToInt(String s) {
+
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int sum = 0;
+        int i = s.length()-1;
+
+        while(i >= 0){
+
+            char ch = s.charAt(i);
+            boolean subtracted = false;
+
+            if(ch=='I' && i < s.length()-1){
+                if(s.charAt(i+1)=='V' || s.charAt(i+1)=='X'){
+                    sum -= 1;
+                    subtracted = true;
+                }
+            }
+
+            else if(ch=='X' && i < s.length()-1){
+                if(s.charAt(i+1)=='L' || s.charAt(i+1)=='C'){
+                    sum -= 10;
+                    subtracted = true;
+                }
+            }
+
+            else if(ch=='C' && i < s.length()-1){
+                if(s.charAt(i+1)=='D' || s.charAt(i+1)=='M'){
+                    sum -= 100;
+                    subtracted = true;
+                }
+            }
+
+            if(!subtracted){
+                sum += map.get(ch);
+            }
+
+            i--;
+        }
+
+        return sum;
+    }
+}
